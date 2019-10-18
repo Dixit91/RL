5ad619c4c9f8ac1722f6ec5be842a809e7ee9123
Dixit91 <41976491+Dixit91@users.noreply.github.com>
2019-10-18 15:23:45 +0800

    Add files via upload

---
+++ MC.py
@@ -0,0 +1,244 @@
+####MC Control with Epsilon-Greedy Policies Solution
+%matplotlib inline
+
+import gym
+import matplotlib
+import numpy as np
+import sys
+
+from collections import defaultdict
+if "../" not in sys.path:
+  sys.path.append("../") 
+from lib.envs.blackjack import BlackjackEnv
+from lib import plotting
+
+matplotlib.style.use('ggplot')
+
+env = BlackjackEnv()
+
+def make_epsilon_greedy_policy(Q, epsilon, nA):
+    """
+    Creates an epsilon-greedy policy based on a given Q-function and epsilon.
+    
+    Args:
+        Q: A dictionary that maps from state -> action-values.
+            Each value is a numpy array of length nA (see below)
+        epsilon: The probability to select a random action . float between 0 and 1.
+        nA: Number of actions in the environment.
+    
+    Returns:
+        A function that takes the observation as an argument and returns
+        the probabilities for each action in the form of a numpy array of length nA.
+    
+    """
+    def policy_fn(observation):
+        A = np.ones(nA, dtype=float) * epsilon / nA
+        best_action = np.argmax(Q[observation])
+        A[best_action] += (1.0 - epsilon)
+        return A
+    return policy_fn
+	
+def mc_control_epsilon_greedy(env, num_episodes, discount_factor=1.0, epsilon=0.1):
+    """
+    Monte Carlo Control using Epsilon-Greedy policies.
+    Finds an optimal epsilon-greedy policy.
+    
+    Args:
+        env: OpenAI gym environment.
+        num_episodes: Number of episodes to sample.
+        discount_factor: Gamma discount factor.
+        epsilon: Chance the sample a random action. Float betwen 0 and 1.
+    
+    Returns:
+        A tuple (Q, policy).
+        Q is a dictionary mapping state -> action values.
+        policy is a function that takes an observation as an argument and returns
+        action probabilities
+    """
+    
+    # Keeps track of sum and count of returns for each state
+    # to calculate an average. We could use an array to save all
+    # returns (like in the book) but that's memory inefficient.
+    returns_sum = defaultdict(float)
+    returns_count = defaultdict(float)
+    
+    # The final action-value function.
+    # A nested dictionary that maps state -> (action -> action-value).
+	#Q[s][a]表示在状态s下执行动作a后的Q值。
+	#每一个状态对应一个Q向量,Q向量的维度是动作空间的大小,Q向量中的值表示对应动作的Q值。
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    
+    # policy是策略函数,输入状态,返回此状态下动作执行概率向量,向量的维度是动作空间的大小。
+    policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
+    
+    for i_episode in range(1, num_episodes + 1):
+        # Print out which episode we're on, useful for debugging.
+        if i_episode % 1000 == 0:
+            print("\rEpisode {}/{}.".format(i_episode, num_episodes), end="")
+            sys.stdout.flush()
+
+        # Generate an episode.
+        # episode是一个列表,其中的元素是(s,a,r)
+		#列表中第一个元素是环境反馈的初始状态,第二个元素是执行动作a后环境反馈的下一个状态...
+        episode = []
+        state = env.reset()
+        for t in range(100):
+            probs = policy(state)
+            action = np.random.choice(np.arange(len(probs)), p=probs)
+            next_state, reward, done, _ = env.step(action)
+            episode.append((state, action, reward))
+            if done:
+                break
+            state = next_state
+
+        # Find all (state, action) pairs we've visited in this episode
+        # We convert each state to a tuple so that we can use it as a dict key
+        sa_in_episode = set([(tuple(x[0]), x[1]) for x in episode])
+        for state, action in sa_in_episode:
+            sa_pair = (state, action)
+            # x[0]是状态,x[1]是动作,x[2]是奖励
+            first_occurence_idx = next(i for i,x in enumerate(episode)
+                                       if x[0] == state and x[1] == action)
+            # Sum up all rewards since the first occurance
+            G = sum([x[2]*(discount_factor**i) for i,x in enumerate(episode[first_occurence_idx:])])
+            #每次访问蒙特卡洛策略评估
+			# 对于所有的episode,每个episode出现的所有同一个状态-动作对的G求均值
+            returns_sum[sa_pair] += G
+            returns_count[sa_pair] += 1.0
+            Q[state][action] = returns_sum[sa_pair] / returns_count[sa_pair]
+        
+        # The policy is improved implicitly by changing the Q dictionary
+    
+    return Q, policy
+	
+Q, policy = mc_control_epsilon_greedy(env, num_episodes=500000, epsilon=0.1)
+
+# For plotting: Create value function from action-value function
+# by picking the best action at each state
+V = defaultdict(float)
+for state, actions in Q.items():
+    action_value = np.max(actions)
+    V[state] = action_value
+plotting.plot_value_function(V, title="Optimal Value Function")
+
+
+#####Off-Policy MC Control with Weighted Importance Sampling Solution
+def create_random_policy(nA):
+    """
+    Creates a random policy function.
+    
+    Args:
+        nA: Number of actions in the environment.
+    
+    Returns:
+        A function that takes an observation as input and returns a vector
+        of action probabilities
+    """
+    A = np.ones(nA, dtype=float) / nA
+    def policy_fn(observation):
+        return A
+    return policy_fn
+
+def create_greedy_policy(Q):
+    """
+    Creates a greedy policy based on Q values.
+    
+    Args:
+        Q: A dictionary that maps from state -> action values
+        
+    Returns:
+        A function that takes an observation as input and returns a vector
+        of action probabilities.
+    """
+    
+    def policy_fn(state):
+        A = np.zeros_like(Q[state], dtype=float)
+        best_action = np.argmax(Q[state])
+        A[best_action] = 1.0
+        return A
+    return policy_fn
+	
+def mc_control_importance_sampling(env, num_episodes, behavior_policy, discount_factor=1.0):
+    """
+    Monte Carlo Control Off-Policy Control using Weighted Importance Sampling.
+    Finds an optimal greedy policy.
+    
+    Args:
+        env: OpenAI gym environment.
+        num_episodes: Number of episodes to sample.
+        behavior_policy: The behavior to follow while generating episodes.
+            A function that given an observation returns a vector of probabilities for each action.
+        discount_factor: Gamma discount factor.
+    
+    Returns:
+        A tuple (Q, policy).
+        Q is a dictionary mapping state -> action values.
+        policy is a function that takes an observation as an argument and returns
+        action probabilities. This is the optimal greedy policy.
+    """
+    
+    # The final action-value function.
+    # A dictionary that maps state -> action values
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    # The cumulative denominator of the weighted importance sampling formula
+    # (across all episodes)
+    C = defaultdict(lambda: np.zeros(env.action_space.n))
+    
+    # Our greedily policy we want to learn
+	#目标策略
+    target_policy = create_greedy_policy(Q)
+        
+    for i_episode in range(1, num_episodes + 1):
+        # Print out which episode we're on, useful for debugging.
+        if i_episode % 1000 == 0:
+            print("\rEpisode {}/{}.".format(i_episode, num_episodes), end="")
+            sys.stdout.flush()
+
+        # Generate an episode.
+        # An episode is an array of (state, action, reward) tuples
+        episode = []
+        state = env.reset()
+        for t in range(100):
+            # Sample an action from our policy
+			#行为策略
+            probs = behavior_policy(state)
+            action = np.random.choice(np.arange(len(probs)), p=probs)
+            next_state, reward, done, _ = env.step(action)
+            episode.append((state, action, reward))
+            if done:
+                break
+            state = next_state
+        
+        # Sum of discounted returns
+        G = 0.0
+        # The importance sampling ratio (the weights of the returns)
+        W = 1.0
+        # For each step in the episode, backwards
+        for t in range(len(episode))[::-1]:
+            state, action, reward = episode[t]
+            # Update the total reward since step t
+            G = discount_factor * G + reward
+            # Update weighted importance sampling formula denominator
+            C[state][action] += W
+            # Update the action-value function using the incremental update formula (5.7)
+            # This also improves our target policy which holds a reference to Q
+            Q[state][action] += (W / C[state][action]) * (G - Q[state][action])
+            # If the action taken by the behavior policy is not the action 
+            # taken by the target policy the probability will be 0 and we can break
+            if action !=  np.argmax(target_policy(state)):
+                break
+            W = W * 1./behavior_policy(state)[action]
+        
+    return Q, target_policy
+	
+random_policy = create_random_policy(env.action_space.n)
+Q, policy = mc_control_importance_sampling(env, num_episodes=500000, behavior_policy=random_policy)
+
+# For plotting: Create value function from action-value function
+# by picking the best action at each state
+V = defaultdict(float)
+for state, action_values in Q.items():
+    action_value = np.max(action_values)
+    V[state] = action_value
+plotting.plot_value_function(V, title="Optimal Value Function")
+
